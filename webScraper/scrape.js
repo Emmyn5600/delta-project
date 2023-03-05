e321650708e9609f5434d6982f240fff98811f70
emmyn5600 <emmy66418@gmail.com>
2023-03-05 23:24:04 +0200

    adding scraper server

---
+++ webScraper/scrape.js
@@ -0,0 +1,35 @@
+const express = require("express")
+const axios  = require("axios")
+const Cheerio = require("cheerio")
+
+const app = express() 
+
+ const port = 8080
+
+ let data=[]
+
+ const url = "https://en.wikipedia.org/wiki/Silicon_Valley"
+
+ const getData = async ()=>{
+  try {
+    
+    let res = await axios.get(url)
+    let $ = await Cheerio.load(res.data)
+    $("#mw-content-text > div.mw-parser-output > div:nth-child(92) > ul >li >a"
+    ).each((i,e)=>{
+      data.push($(e).text().trim())
+    })
+  } catch (error) {
+    console.log(error)
+  }
+ }
+
+ getData()
+
+ app.get("/data",(req,res)=>{
+  
+  res.status(200).send({type:"Notable companies in Silicon Valley.",data})
+ })
+
+
+ app.listen(port,()=>console.log("server is running ")) 
