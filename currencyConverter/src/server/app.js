a9daeaa1f71cd682a159622240e8acc6caf6ef22
emmyn5600 <emmy66418@gmail.com>
2023-03-08 00:28:49 +0200

    adding route to get all currency signs

--- currencyConverter/src/server/app.js
+++ currencyConverter/src/server/app.js
@@ -2,12 +2,29 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+const api_key = 'f116e8aa34dab3d00338878f4d963574';
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const response = await fetch(`https://api.currencyscoop.com/v1/latest?api_key=${api_key}`);
+  const data = await response.json();
+  const currencies = Object.keys(data.response.rates);
+  console.log(currencies);
+  // res.render('index', { currencies });
 });
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
-});
+
+
+
+
+
+
+
+
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server listening on port ${port}`));
