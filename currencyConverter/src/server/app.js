a4843fdcd5e5d45a2ddfaba6e3daf3b96f0e2f8e
emmyn5600 <emmy66418@gmail.com>
2023-03-08 00:22:18 +0200

    adding server and view

---
+++ currencyConverter/src/server/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
