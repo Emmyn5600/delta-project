261660defcb1596baacb3a04f99d3bb3c7526887
emmyn5600 <emmy66418@gmail.com>
2023-03-07 22:15:08 +0200

    adding api key

--- weatherApp/src/App.tsx
+++ weatherApp/src/App.tsx
@@ -1,4 +1,9 @@
-import { useState } from "react";
+import React, { useState } from 'react';
+const api = {
+  key: "7b1bf55e825c99fc08d939795182a6cf",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
 
 import "./App.css";
 
