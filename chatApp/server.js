0a5dd440560199bdf3ca5fe3c7ff62c3aa1ee56b
emmyn5600 <emmy66418@gmail.com>
2023-03-09 19:05:33 +0200

    init chat app

---
+++ chatApp/server.js
@@ -0,0 +1,37 @@
+// make server 
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+const http = require('http')
+const socketio = require('socket.io')
+
+//create server
+const server = http.createServer(app);
+
+//create socket
+const io = socketio(server);
+
+
+io.on('connection', (socket) => {
+  console.log('New WebSocket connection')
+
+  socket.emit('message', 'Welcome! to chat app')
+
+  socket.broadcast.emit('message', 'A new user has joined')
+
+  socket.on("disconnect", () => {
+    io.emit("message", "A user has left");
+  } );
+})
+//set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+
+
+
+
+server.listen(port, () => console.log(`Example app listening on port ${port}!`));
