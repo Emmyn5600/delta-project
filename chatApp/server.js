f5dd49c572c63f66583d620377286a8093d2888e
emmyn5600 <emmy66418@gmail.com>
2023-03-09 19:29:02 +0200

    adding format messages function

--- chatApp/server.js
+++ chatApp/server.js
@@ -5,6 +5,7 @@ const app = express();
 const port = 3000;
 const http = require('http')
 const socketio = require('socket.io')
+const formatMessage = require('./utils/messages')
 
 //set static folder
 app.use(express.static(path.join(__dirname, 'public')));
@@ -15,20 +16,21 @@ const server = http.createServer(app);
 //create socket
 const io = socketio(server);
 
+const userName = 'ChatCord Bot'
 
 io.on('connection', (socket) => {
   console.log('New WebSocket connection')
 
-  socket.emit('message', 'Welcome! to chat app')
+  socket.emit('message', formatMessage(userName,'Welcome! to chat app') )
 
-  socket.broadcast.emit('message', 'A new user has joined')
+  socket.broadcast.emit('message',formatMessage(userName, 'A new user has joined'))
 
   socket.on("disconnect", () => {
-    io.emit("message", "A user has left");
+    io.emit("message",formatMessage(userName, "A user has left"));
   } );
 
   socket.on('chatMessage', (message) => {
-    io.emit('message', message)
+    io.emit('message',formatMessage("userName", message))
   })
 })
 
