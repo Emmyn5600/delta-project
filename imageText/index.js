1f04b3c426bdb6a7a117719bcc50f34bed20d7f1
emmyn5600 <emmy66418@gmail.com>
2023-03-13 10:09:02 +0200

    final app

--- imageText/index.js
+++ imageText/index.js
@@ -4,12 +4,32 @@ const fs = require("fs")
 //make public folder accessible
 // app.use(express.static("public"))
 
-//upload image
-tesseract.recognize("eng_bw.png", "eng", {
-  logger: m => console.log(m)
-}).then(result => {
-  console.log(result.data.text)
-}).catch(err => {
-  console.log(err.message)
-})
+//text recognition
 
+const getImageText = async (fileName, lang, logger) => {
+  let recognizeResult = null
+  try {
+      if (fs.existsSync(fileName)) {
+          if (logger) {
+              const myLogger = {
+                  logger: m => console.log(m)
+              }
+              recognizeResult = await tesseract.recognize(fileName, lang, myLogger)
+          } else {
+              recognizeResult = await tesseract.recognize(fileName, lang)
+          }
+          if (recognizeResult) {
+              return recognizeResult.data.text
+          }
+      }
+  } catch (error) {
+      return error.message
+  }
+}
+
+
+getImageText("testimage.png", "eng", false).then(text => {
+    console.log(text)
+}).catch(errMsg => {
+    console.log(errMsg)
+})
