574c8398632bafe78bba9c3efbaf6a4713ba2f71
emmyn5600 <emmy66418@gmail.com>
2023-03-15 14:57:54 +0200

    adding server

--- pdfTextReader/reader.py
+++ pdfTextReader/reader.py
@@ -1,9 +1,13 @@
 import PyPDF2
+import sys
 
-def extract_text_pypdf2(filename):
+def extract_text(filename):
     with open(filename, 'rb') as f:
         reader = PyPDF2.PdfFileReader(f)
         text = ''
         for page in reader.pages:
             text += page.extractText()
     return text
+
+text = extract_text(sys.argv[1])
+print(text)
